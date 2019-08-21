7a7ad18366fa55b17b045bf68e3a98becbce7abd
Proletter <oluwaseunismaila0@gmail.com>
2019-08-21 12:49:41 +0100

    new commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const data = [
+    {"name": "Ade",
+     "Job": "Policeman",
+     "Course": "Mbbs",
+    },
+    {"name": "Ade",
+    "Job": "Banker",
+    "Course": "Maths",
+    },
+    {"name": "Ade",
+    "Job": "Banker",
+    "Course": "English",
+    },
+    {"name": "Ade",
+    "Job": "Banker",
+    "Course": "History",
+    },
+    {"name": "Ade",
+    "Job": "Banker",
+    "Course": "ELect-elect",
+    },
+    {"name": "Ade",
+    "Job": "Banker",
+    "Course": "Pol Sci",
+    },
+    {"name": "Ade",
+    "Job": "Banker",
+    "Course": "Chemistry",
+    },
+];
+
+const namefield = document.getElementById("Name").innerHTML;
+const courseField = document.getElementById("Course").innerHTML;
+const jobField = document.getElementById("Job").innerHTML;
+
+
